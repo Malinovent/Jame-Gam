c5eb48de321f9df8d1fa117fda4eb7f33dd1147f
JP Provost <jphil.provost@outlook.com>
2023-06-03 18:11:14 -0400

    Added placeholder code for switching to animations

--- Assets/Painting/PaintBrushCursor.cs
+++ Assets/Painting/PaintBrushCursor.cs
@@ -21,11 +21,13 @@ public class PaintBrushCursor : MonoBehaviour
     private void OnEnable()
     {
         BrushManager.OnBrushStateChanged += UpdateAnimation;
+        BrushManager.OnColorChanged += UpdateAnimation;
     }
 
     private void OnDisable()
     {
         BrushManager.OnBrushStateChanged -= UpdateAnimation;
+        BrushManager.OnColorChanged -= UpdateAnimation;
     }
 
     // Start is called before the first frame update
@@ -43,10 +45,82 @@ public class PaintBrushCursor : MonoBehaviour
     }
 
     private void UpdateAnimation(BrushStates brushState)
-    { 
-        if(brushState == BrushStates.PAINTING)
+    {
+        if (brushState == BrushStates.PAINTING)
         {
-            
+            switch (BrushManager.CurrentBrushColor)
+            {
+                case ColorsEnum.RED:
+                    //Change animation to red painting
+                    break;
+                case ColorsEnum.BLUE:
+                    //Change animation to blue painting
+                    break;
+                case ColorsEnum.GREEN:
+                    //Change animation to green painting
+                    break;
+                case ColorsEnum.YELLOW:
+                    //Change animation to yellow painting
+                    break;
+            }
+        }
+        else
+        {
+            switch (BrushManager.CurrentBrushColor)
+            {
+                case ColorsEnum.RED:
+                    //Change animation to red idle
+                    break;
+                case ColorsEnum.BLUE:
+                    //Change animation to blue idle
+                    break;
+                case ColorsEnum.GREEN:
+                    //Change animation to green idle
+                    break;
+                case ColorsEnum.YELLOW:
+                    //Change animation to yellow idle
+                    break;
+            }
+        }
+    }
+
+    private void UpdateAnimation(ColorsEnum color)
+    {
+        if (BrushManager.CurrentBrushState == BrushStates.PAINTING)
+        {
+            switch (BrushManager.CurrentBrushColor)
+            {
+                case ColorsEnum.RED:
+                    //Change animation to red painting
+                    break;
+                case ColorsEnum.BLUE:
+                    //Change animation to blue painting
+                    break;
+                case ColorsEnum.GREEN:
+                    //Change animation to green painting
+                    break;
+                case ColorsEnum.YELLOW:
+                    //Change animation to yellow painting
+                    break;
+            }
+        }
+        else
+        {
+            switch (BrushManager.CurrentBrushColor)
+            {
+                case ColorsEnum.RED:
+                    //Change animation to red idle
+                    break;
+                case ColorsEnum.BLUE:
+                    //Change animation to blue idle
+                    break;
+                case ColorsEnum.GREEN:
+                    //Change animation to green idle
+                    break;
+                case ColorsEnum.YELLOW:
+                    //Change animation to yellow idle
+                    break;
+            }
         }
     }
 }
